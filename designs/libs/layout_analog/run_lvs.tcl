386083cf60a4b92006fea00c22bcd34a2f413ab0
Angle17 <angelg.romero417@gmail.com>
2025-09-10 13:30:54 -0400

    Revert "working on lvs"
    This reverts commit 0751b2405ecad17acec38277334e61d29af9c0ff.

--- designs/libs/layout_analog/run_lvs.tcl
+++ designs/libs/layout_analog/run_lvs.tcl
@@ -5,8 +5,7 @@ if {[catch {set PDK $::env(PDK)}]} {set PDK gf180mcuD}
 
 set pdklib ${PDK_ROOT}/${PDK}
 set techlibs ${pdklib}/libs.tech
-
-set reflibs /foss/designs/libs/layout_analog/libs_new.ref
+set reflibs ${pdklib}/libs.ref
 
 set setupfile gf180mcuD_setup.tcl
 set sclib ${reflibs}/gf180mcu_fd_sc_mcu9t5v0/spice/gf180mcu_fd_sc_mcu9t5v0.spice
