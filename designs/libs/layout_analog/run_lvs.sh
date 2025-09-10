face836957e474b9c92d855c5a44027a258d1e36
Angle17 <angelg.romero417@gmail.com>
2025-09-09 20:17:20 -0400

    updated script

--- designs/libs/layout_analog/run_lvs.sh
+++ designs/libs/layout_analog/run_lvs.sh
@@ -81,7 +81,8 @@ if {[catch {set PDK \$::env(PDK)}]} {set PDK gf180mcuD}
 
 set pdklib \${PDK_ROOT}/\${PDK}
 set techlibs \${pdklib}/libs.tech
-set reflibs \${pdklib}/libs.ref
+
+set reflibs /foss/designs/libs/layout_analog/libs_new.ref
 
 set setupfile ${PDK}_setup.tcl
 set sclib \${reflibs}/gf180mcu_fd_sc_mcu9t5v0/spice/gf180mcu_fd_sc_mcu9t5v0.spice
