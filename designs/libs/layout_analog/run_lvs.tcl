6005dc14dbd50c6b100b4f35ae8f3e987cae4cd6
Christopher Amankwaa <christopheramankwaa@Christophers-MacBook-Pro-3029.local>
2025-09-11 14:21:34 -0500

    lvs and drc clean designs

--- designs/libs/layout_analog/run_lvs.tcl
+++ designs/libs/layout_analog/run_lvs.tcl
@@ -1,4 +1,4 @@
-# Tcl script to run LVS on triangle_osc
+# Tcl script to run LVS on opamp_n_input
 
 if {[catch {set PDK_ROOT $::env(PDK_ROOT)}]} {set PDK_ROOT /usr/share/pdk}
 if {[catch {set PDK $::env(PDK)}]} {set PDK gf180mcuD}
@@ -11,10 +11,10 @@ set reflibs /foss/designs/libs/layout_analog/libs_new.ref
 set setupfile gf180mcuD_setup.tcl
 set sclib ${reflibs}/gf180mcu_fd_sc_mcu9t5v0/spice/gf180mcu_fd_sc_mcu9t5v0.spice
 
-set circuit1 [readnet spice triangle_osc_layout.spice]
+set circuit1 [readnet spice opamp_n_input_layout.spice]
 set circuit2 [readnet spice $sclib]
 
-readnet spice triangle_osc.spice $circuit2
+readnet spice opamp_n_input.spice $circuit2
 
-lvs "$circuit1 triangle_osc" "$circuit2 triangle_osc" \
-        $setupfile triangle_osc_comp.out
+lvs "$circuit1 opamp_n_input" "$circuit2 opamp_n_input" \
+        $setupfile opamp_n_input_comp.out
