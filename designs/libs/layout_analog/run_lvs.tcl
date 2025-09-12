c0942c51024bc05eb9257cb8aa74b0798e29ab96
Angle17 <angelg.romero417@gmail.com>
2025-09-12 10:01:48 -0400

    clkgen passes LVS

--- designs/libs/layout_analog/run_lvs.tcl
+++ designs/libs/layout_analog/run_lvs.tcl
@@ -1,4 +1,4 @@
-# Tcl script to run LVS on Angel_swmatrix_block
+# Tcl script to run LVS on NO_ClkGen
 
 if {[catch {set PDK_ROOT $::env(PDK_ROOT)}]} {set PDK_ROOT /usr/share/pdk}
 if {[catch {set PDK $::env(PDK)}]} {set PDK gf180mcuD}
@@ -11,10 +11,10 @@ set reflibs /foss/designs/libs/layout_analog/libs_new.ref
 set setupfile gf180mcuD_setup.tcl
 set sclib ${reflibs}/gf180mcu_fd_sc_mcu9t5v0/spice/gf180mcu_fd_sc_mcu9t5v0.spice
 
-set circuit1 [readnet spice Angel_swmatrix_block_layout.spice]
+set circuit1 [readnet spice NO_ClkGen_layout.spice]
 set circuit2 [readnet spice $sclib]
 
-readnet spice Angel_swmatrix_block.spice $circuit2
+readnet spice NO_ClkGen.spice $circuit2
 
-lvs "$circuit1 Angel_swmatrix_block" "$circuit2 Angel_swmatrix_block" \
-        $setupfile Angel_swmatrix_block_comp.out
+lvs "$circuit1 NO_ClkGen" "$circuit2 NO_ClkGen" \
+        $setupfile NO_ClkGen_comp.out
