695d903cb8e945c00e8d753083c190fe795f3c97
gloverdevlinr <devlin_glover@brown.edu>
2025-09-28 13:32:58 -0700

    lvs completed but not running, labels added to gds file

--- designs/libs/layout_analog/run_lvs.tcl
+++ designs/libs/layout_analog/run_lvs.tcl
@@ -1,6 +1,4 @@
-
-# Tcl script to run LVS on Angel_swmatrix_block
-
+# Tcl script to run LVS on M15_XDAC_Layout
 
 if {[catch {set PDK_ROOT $::env(PDK_ROOT)}]} {set PDK_ROOT /usr/share/pdk}
 if {[catch {set PDK $::env(PDK)}]} {set PDK gf180mcuD}
@@ -13,10 +11,10 @@ set reflibs /foss/designs/libs/layout_analog/libs_new.ref
 set setupfile gf180mcuD_setup.tcl
 set sclib ${reflibs}/gf180mcu_fd_sc_mcu9t5v0/spice/gf180mcu_fd_sc_mcu9t5v0.spice
 
-set circuit1 [readnet spice opamp_n_input_layout.spice]
+set circuit1 [readnet spice M15_XDAC_Layout_layout.spice]
 set circuit2 [readnet spice $sclib]
 
-readnet spice opamp_n_input.spice $circuit2
+readnet spice M15_XDAC_Layout.spice $circuit2
 
-lvs "$circuit1 opamp_n_input" "$circuit2 opamp_n_input" \
-        $setupfile opamp_n_input_comp.out
+lvs "$circuit1 M15_XDAC_Layout" "$circuit2 M15_XDAC_Layout" \
+        $setupfile M15_XDAC_Layout_comp.out
