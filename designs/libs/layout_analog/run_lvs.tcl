806fcc46b3b805bc955572c38089a70cf4b4dd00
Christopher Amankwaa <christopheramankwaa@Christophers-MacBook-Pro-3029.local>
2025-09-29 05:59:13 -0500

    Commercial DRC clean Layout

--- designs/libs/layout_analog/run_lvs.tcl
+++ designs/libs/layout_analog/run_lvs.tcl
@@ -1,4 +1,4 @@
-# Tcl script to run LVS on M15_XDAC_Layout
+# Tcl script to run LVS on op_Deadtime_Driver
 
 if {[catch {set PDK_ROOT $::env(PDK_ROOT)}]} {set PDK_ROOT /usr/share/pdk}
 if {[catch {set PDK $::env(PDK)}]} {set PDK gf180mcuD}
@@ -11,10 +11,10 @@ set reflibs /foss/designs/libs/layout_analog/libs_new.ref
 set setupfile gf180mcuD_setup.tcl
 set sclib ${reflibs}/gf180mcu_fd_sc_mcu9t5v0/spice/gf180mcu_fd_sc_mcu9t5v0.spice
 
-set circuit1 [readnet spice M15_XDAC_Layout_layout.spice]
+set circuit1 [readnet spice op_Deadtime_Driver_layout.spice]
 set circuit2 [readnet spice $sclib]
 
-readnet spice M15_XDAC_Layout.spice $circuit2
+readnet spice op_Deadtime_Driver.spice $circuit2
 
-lvs "$circuit1 M15_XDAC_Layout" "$circuit2 M15_XDAC_Layout" \
-        $setupfile M15_XDAC_Layout_comp.out
+lvs "$circuit1 op_Deadtime_Driver" "$circuit2 op_Deadtime_Driver" \
+        $setupfile op_Deadtime_Driver_comp.out
